ba0d16661e5b334ff6ca8f2193d9524a076c88d6
Fionn Duffy <fionnduffy@macbook-pro-2.local>
2018-11-01 15:07:47 +0000

    Initial Commit

---
+++ xcode8gitexample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode8gitexample
+//
+//  Created by Fionn Duffy on 01/11/2018.
+//  Copyright © 2018 Fionn Duffy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
